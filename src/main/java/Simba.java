a24000790ed0a59f7a725ca1aab824c2547e18c7
AishaAydin <ayseydin13@gmail.com>
2020-11-01 21:58:01 -0500

    Lily

--- src/main/java/Simba.java
+++ src/main/java/Simba.java
@@ -1,5 +1,6 @@
 public class Simba {
     public static void main(String[] args) {
         System.out.println("Welcome to the my house little simba!!");
+        System.out.println("Welcome to our family Lily!!");
     }
 }
